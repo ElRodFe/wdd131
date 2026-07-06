5da293d1291b76c133be813f7e0dfe0d06bf366a
ElRodFe <el.rod.fe@gmail.com>
2026-07-06 19:21:28 -0300

    02 Learning Activity: JavaScript Debugging (Finished)

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,16 @@
+let radius = 10;
+let area = 0;
+
+let radiusOutput = document.textContent = radius;
+let areaOutput = document.textContent = area;
+
+const PI = 3.14159;
+
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
+
+radius = 20;
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
