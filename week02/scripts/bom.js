ae6a2a30836d191bce1247b769662a55f604f7ae
ElRodFe <el.rod.fe@gmail.com>
2026-07-06 12:49:40 -0300

    W02 Learning Activity: Handling DOM Events (Finished)

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,15 +3,26 @@ const input = document.getElementById("favchap");
 const button = document.querySelector("button");
 const list = document.getElementById("list");
 
-//Create new HTML elements
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+//Event listener for adding the chapter to the list
+button.addEventListener("click", function() {
+    if (input.value.trim() !== "") {
+        //Create new HTML elements
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
 
-//Change created elements's content
-li.textContent = input.value;
-deleteButton.textContent = "❌";
+        //Change created elements's content
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-//Append created elements to existing ones
-li.append(deleteButton);
-list.append(li);
+        //Append created elements to existing ones
+        li.append(deleteButton);
+        list.append(li);
+        input.value = "";
 
+        //Delete the added chapter
+        deleteButton.addEventListener("click", function() {
+        list.removeChild(li);
+        input.focus();
+        })
+    }
+}) 
