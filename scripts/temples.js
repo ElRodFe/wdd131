2aaa7bd5ab4b9ef45bd1f567feda1232ed4c27f5
ElRodFe <el.rod.fe@gmail.com>
2026-07-08 13:10:40 -0300

    W02 Assignment: Picture Album (Finished)

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,12 @@
+const hamButton = document.querySelector("#menu");
+const nav = document.querySelector("nav");
 
+hamButton.addEventListener("click", function() {
+    nav.classList.toggle("open");
+
+    if (nav.classList.contains("open")) {
+        hamButton.textContent = "x"
+    } else {
+        hamButton.textContent = "☰"
+    }
+});
