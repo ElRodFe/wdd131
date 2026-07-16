c8243d365ca6968aa93c18ffd65d25714436acdb
ElRodFe <el.rod.fe@gmail.com>
2026-07-16 12:56:51 -0300

    W03 Assignment: Country Page (css added, html and js improved. work finished)

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -18,24 +18,8 @@ function calculateCelsiusWindChill(t, w) {
     return "N/A";
 }
 
-//Get the weather div element
-const weatherCard = document.querySelector(".weather");
-
-//create and populate the paragraphs with info
-const pTemp = document.createElement("p");
-pTemp.innerHTML = `<strong>Temperature:</strong> ${temp}°C`
-
-const pCoonditions = document.createElement("p");
-pCoonditions.innerHTML = `<strong>Conidtions:</strong> ${conditions}`
-
-const pWind = document.createElement("p");
-pWind.innerHTML = `<strong>Wind:</strong> ${wind}Km/h`
-
-const pWchill = document.createElement("p");
-pWchill.innerHTML = `<strong>WindChill:</strong> ${calculateCelsiusWindChill(temp, wind)}°C`
-
-//Append created paragraphs to weather div element
-weatherCard.appendChild(pTemp);
-weatherCard.appendChild(pCoonditions);
-weatherCard.appendChild(pWind);
-weatherCard.appendChild(pWchill);
+//Add weather info to weather div element
+document.getElementById("temp").textContent = `${temp}°C`;
+document.getElementById("conditions").textContent = conditions;
+document.getElementById("wind").textContent = `${wind} km/h`;
+document.getElementById("windChill").textContent = calculateCelsiusWindChill(temp, wind);
